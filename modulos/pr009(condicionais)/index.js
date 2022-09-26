1e0486f847e215b236613e23191864eb9bb0c5ae
Felipe Dalbert <felipeoficial195@gmail.com>
2022-09-26 17:48:06 -0300

    atualização de práticas e execicio

---
+++ modulos/pr009(condicionais)/index.js
@@ -0,0 +1,15 @@
+let nomeFilme = 'vingadores';
+
+// if(nomeFilme === 'Vingadores'){
+//     console.log('É o filme dos vingadores');
+// }else if(nomeFilme === 'Batman'){
+//     console.log('É o filme do Batman');
+// }else{
+//     console.log('É outro filme');
+// }
+
+//operador ternário
+
+false ? console.log('condição verdadeira') : console.log('condição falsa');
+
+nomeFilme = 'Vingadores' ? console.log('É o filme dos vingadores') : console.log('É outro filme')
---
+++ modulos/pr011(funcoes)/index.js
@@ -0,0 +1,32 @@
+function testeFuncao(){
+    console.log('Testando função sem parametro')
+}
+
+testeFuncao();
+
+function soma(n1, n2){
+    console.log(n1 + n2)
+}
+
+soma(4, 5);
+
+function retornaValor(n1, n2){
+    return n1 + n2
+}
+
+console.log(retornaValor(2, 8));
+
+resultadoDaSomaReturn = retornaValor(8, 8);
+
+console.log(resultadoDaSomaReturn);
+
+resultadoDaSomaReturn = retornaValor(resultadoDaSomaReturn, 2);
+
+console.log(resultadoDaSomaReturn);
+
+
+function incetivarQuester(nomeQuester = 'Quester'){
+    console.log('Muito bem ' + nomeQuester + ', você chegou ao JS, continue assim, continue!');
+}
+
+incetivarQuester('Daniel');
