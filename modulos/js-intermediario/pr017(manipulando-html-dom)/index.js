5479847eb18cb4c9070040c6596d32958f558e06
Felipe Dalbert <felipeoficial195@gmail.com>
2022-10-06 17:51:30 -0300

    aulas sobre dom

---
+++ modulos/js-intermediario/pr017(manipulando-html-dom)/index.js
@@ -0,0 +1,9 @@
+function adicionarComentario(){
+    let inputComentario = document.getElementsByName('novo-comentario')
+
+    console.log(inputComentario)
+
+    let textoDigitado = inputComentario[0].value
+
+    console.log(textoDigitado)
+}
