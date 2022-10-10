d9bfecdf32076ca3c042f9f3d57a395fc7e1a151
Felipe Dalbert <felipeoficial195@gmail.com>
2022-10-10 17:50:04 -0300

    mais aulas sobre dom

--- modulos/js-intermediario/pr017(manipulando-html-dom)/index.js
+++ modulos/js-intermediario/pr017(manipulando-html-dom)/index.js
@@ -6,4 +6,8 @@ function adicionarComentario(){
     let textoDigitado = inputComentario[0].value
 
     console.log(textoDigitado)
+
+    let novosComentarios = document.getElementById('novos-comentarios')
+
+    novosComentarios.innerHTML += `<p>Novo comentário: ${textoDigitado}<p>`
 }
