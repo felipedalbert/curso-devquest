509676cdb3db8aacd6c48dc5b8772097a89db89b
Felipe Dalbert <felipeoficial195@gmail.com>
2023-02-18 00:49:22 -0300

    nova aula sobre promises

---
+++ modulos/12-js-avancado/pr010(promises)/index.js
@@ -0,0 +1,25 @@
+/*
+    Pendente - Estado inicial (promise criada, mas não executada)
+    Realizada - Sucesso na execução (resolve)
+    Recusada - Falha na execução da promise (reject)
+    Estabelecida - Promise realizada ou recusada
+*/
+
+
+
+let ferverAgua = (chaleiraEstaNoFogao, fogaoEstaligado) => {
+    return new Promise((resolve, reject)=>{
+        if(chaleiraEstaNoFogao && fogaoEstaligado){
+            console.log('Começando o processo de ferver a água')
+            resolve()
+        }else{
+            console.log('É necessário ligar o fogão e colocar a chaleira no fogão para ferver a água')
+            reject()
+        }
+    })
+}
+
+let chaleiraEstaNoFogao = true
+let fogaoEstaligado = false
+ferverAgua(chaleiraEstaNoFogao, fogaoEstaligado)
+console.log('Fazendo café')
