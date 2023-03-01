c2ca10870e38ec8c9a1d032c578efe63dec60ab0
Felipe Dalbert <felipeoficial195@gmail.com>
2023-03-01 02:58:33 -0300

    utilizando deck of cards API e exibindo as cartas tiradas no html

---
+++ modulos/12-js-avancado/pr015(fetch)/cartas.js
@@ -0,0 +1,34 @@
+criarBaralho()
+
+async function criarBaralho(){
+    const resposta = await fetch('https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+    baralho = await resposta.json()
+}
+
+
+async function tirarCartas(deck_id){
+    const resposta =  await fetch(`https://www.deckofcardsapi.com/api/deck/${deck_id}/draw/?count=3`)
+    return await resposta.json()
+}
+
+
+async function mostrarCartaTirada(){
+    let containerCartas= document.getElementsByClassName('container')
+    containerCartas[0].innerHTML = ''
+
+    const cartasTiradas = await tirarCartas(baralho.deck_id)
+    const imagensCartas = cartasTiradas.cards
+    console.log(imagensCartas)
+
+    
+
+    for(let carta of imagensCartas){
+        containerCartas[0].innerHTML += `
+            <img src="${carta.image}" alt="carta">
+        `
+    }
+}
+
+let baralho
+
+document.getElementById('tirar-carta').addEventListener('click', mostrarCartaTirada)
