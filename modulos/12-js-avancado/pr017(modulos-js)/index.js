de25568b58e519a6f67aed509d717e33af2bac67
Felipe Dalbert <felipeoficial195@gmail.com>
2023-03-03 23:27:03 -0300

    aula sobre módulo, exportações e importações (pr017)

---
+++ modulos/12-js-avancado/pr017(modulos-js)/index.js
@@ -0,0 +1,5 @@
+import opMat from './op-mat.js'
+
+console.log(opMat.somar(1,3))
+console.log(opMat.multiplicar(4,5))
+
---
+++ modulos/12-js-avancado/pr017(modulos-js)/op-mat.js
@@ -0,0 +1,13 @@
+export function somar(n1, n2) {
+    return n1 + n2
+}
+
+
+function multiplicar(n1, n2) {
+    return n1 * n2
+}
+
+export default{
+    somar,
+    multiplicar
+}
