ea90ca804f63c87e47c65362a1a14c7c3f9d3bfe
Felipe Dalbert <felipeoficial195@gmail.com>
2023-07-13 13:54:51 -0300

    components life cycle with class (modulo 15 - react-avançado)

---
+++ modulos/15-react-avancado/pr001-estado-no-react/react-estado/src/components/deck-of-cards/deck-of-cards.js
@@ -0,0 +1,57 @@
+import { Component} from "react";
+
+async function creatDeck(){
+    const response = await fetch('https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+    const deck = await response.json()
+    return deck.deck_id
+}
+
+async function getCards(deckId){
+    const response = await fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=52`)
+    return await response.json()
+}
+
+class DeckOfCards extends Component {
+    constructor(){
+        console.log('constructing')
+        super()
+        this.state = {
+            cards: []
+        }
+    }
+
+    async componentDidMount(){
+        console.log('componentDidMount')
+        const deckId = await creatDeck()
+        const deckData = await getCards(deckId)
+
+        this.setState({
+            cards: deckData.cards
+        })
+    }
+
+    render(){
+        console.log('render')
+
+        return (
+            <section>
+                <ul>
+                    {
+                       this.state.cards.map((card, index) => {
+
+                            return(
+                                <li key={index}>
+                                    <img src={card.image} alt={card.value} />
+                                </li> 
+                            )
+                            
+                        }) 
+                    }
+                    
+                </ul>
+            </section>
+        )
+    }
+}
+
+export default DeckOfCards
